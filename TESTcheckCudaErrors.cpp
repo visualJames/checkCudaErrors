6ca0ab4e4a6bb6dac4bbf1ce7deebe3d3b38ae98
a_hoda01 <a_hoda01@uni-muenster.de>
2019-11-09 23:48:38 +0100

    smaller cosmetic changes

--- TESTcheckCudaErrors.cpp
+++ TESTcheckCudaErrors.cpp
@@ -22,17 +22,17 @@ void ErrorPointerThrow() {
     CUdeviceptr d_A;
     try {
       checkCUresultError(cuMemAlloc(&d_A, (unsigned int)nBytes));
-   } catch(CUresultException<(CUresult)1>& e) {
+  } catch(CUresultException<(CUresult)1>& e) {  // CUDA_ERROR_INVALID_VALUE
       std::cout << "Wrong Exception caught" << std::endl;
       std::cout << e.what() << std::endl;
-   } catch(CUresultException<(CUresult)3>& e) {
+  } catch(CUresultException<CUDA_ERROR_NOT_INITIALIZED>& e) {
       std::cout << "Correct Exception caught" << std::endl;
       std::cout << e.what() << std::endl;
-   } catch(std::exception& e) {
-      //Other errors
+  } catch(std::exception& e) {
+      // Other errors
       std::cout << "anderer Error\n";
       std::cout << e.what() << std::endl;
-   }
+  }
 }
 
 void giveColumnsSizeCommandConsole(){
